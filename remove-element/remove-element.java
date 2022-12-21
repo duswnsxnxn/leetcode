b6f5a40b045d64b4a505d7496819245958eb68f6
duswnsxnxn <65449174+duswnsxnxn@users.noreply.github.com>
2022-12-21 17:49:54 +0900

    Time: 0 ms, Memory: 40.8 MB - LeetHub

---
+++ remove-element/remove-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int act = nums.length - 1;
+        int next = 0;
+        
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != val){
+                nums[next] = nums[i];                    
+                next++;
+                }
+            }
+        return next;
+        }
+    }
